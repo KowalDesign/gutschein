d75245c2bbe76f19e96a0ab597afd04f2ab6278f
Karol Kowalczyk <129177103+KowalDesign@users.noreply.github.com>
2023-05-30 06:49:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const copyButton = document.getElementById("copy-button");
 const copyIcon = document.getElementById("copy-icon");
 
-copyButton.addEventListener("click", function () {
+copyButton.addEventListener("click", function (event) {
+    event.preventDefault(); // Verhindert das Standardverhalten des Buttons
+
     const textbox = document.getElementById("textbox");
+    textbox.removeAttribute("readonly"); // Entfernt das readonly-Attribut
     textbox.select();
     document.execCommand("copy");
+    // textbox.setAttribute("readonly", "readonly"); // Setzt das readonly-Attribut wieder
 
     copyIcon.src = "pictures/checkbox.png";
-    
+
     window.open("https://amzn.to/43yL7J4", "_blank");
 });
